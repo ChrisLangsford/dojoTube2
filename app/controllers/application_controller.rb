fa127c6dd172e0958c7082a397072728652b9f50
ChrisLangsford <langz020@gmail.com>
2014-10-30 22:36:52 +0200

    added admin role to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_up) << :admin
   end
 
 end
