340c181a028f4348cbbc1e0a234bd4cf5ba0074d
ChrisLangsford <langz020@gmail.com>
2014-11-01 13:29:54 +0200

    flash messages need configuring, edit of users now works

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -31,9 +31,9 @@ class UserController < ApplicationController
   def update
     params[:user].delete(:password) if params[:user][:password].blank?
     params[:user].delete(:password_confirmation) if params[:user][:password].blank? and params[:user][:password_confirmation].blank?
-    if @user.update_attributes(params[:user])
+    if @user.update_attributes(user_params)
       flash[:notice] = "Successfully updated User."
-      redirect_to root_path
+      redirect_to user_index_path
     else
       render :action => 'edit'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-
-  
-
   root 'welcome#index'
   resources :dojos
 
