0ebf37c2fe591419f62c6f1e90fe545d45931716
ChrisLangsford <langz020@gmail.com>
2014-12-04 11:47:38 +0200

    logout and permissions issue resolved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :role
+    devise_parameter_sanitizer.for(:sign_up) << :adult
+    devise_parameter_sanitizer.for(:sign_up) << :active
+    devise_parameter_sanitizer.for(:sign_up) << :rank
   end
 
 end
