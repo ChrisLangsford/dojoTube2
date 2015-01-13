d759f0fd3e9df859f9b97190aefe548fd0d2731b
ChrisLangsford <langz020@gmail.com>
2015-01-13 13:09:53 +0200

    mail now handled by mandrill app smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,12 +36,14 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    user_name:            'langz020@gmail.com',
-    password:             ENV["EMAIL_PASS"],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => 'dojotube.com', # your domain to identify your server when connecting
+  }
 
 
   # Raises error for missing translations
