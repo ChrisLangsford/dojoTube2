64c7ba69bdac0f130febca9e5e8f2de8bb313f67
ChrisLangsford <langz020@gmail.com>
2015-01-28 16:18:55 +0200

    tweaks

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,5 @@
 class ReportsController < ApplicationController
+	layout 'sidebar'
 	def active_users
 		@active_users = User.all.where(active: "true").count
 		@inactive_users = User.all.count - @active_users
