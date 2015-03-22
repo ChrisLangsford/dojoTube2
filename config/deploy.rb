b264dd91406a4c806248f1a6db0013c8b438fd1c
ChrisLangsford <langz020@gmail.com>
2015-03-22 14:13:14 +0200

    deploy configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :repo_url, 'https://github.com/ChrisLangsford/dojoTube2.git'
  set :pty, true
 
 # Default value for :linked_files is []
- set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+#set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
  set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
