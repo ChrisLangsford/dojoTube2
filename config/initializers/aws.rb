ea9057c178f1a0f8656a2735afd06f80289d2b3f
ChrisLangsford <langz020@gmail.com>
2015-04-06 17:06:01 +0200

    edited aws config

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,7 @@
 AWS.config(access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-			secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'])
+			secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+			region: ENV['S3_REGION'])
+
+AWS.config(logger: Logger.new($stdout), log_level: :debug)
 
 S3_BUCKET = AWS::S3.new.buckets[ENV['S3_BUCKET']]
