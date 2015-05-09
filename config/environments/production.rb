3fe6b5045c44c3ee7c038beb6c94419371ab71f2
ChrisLangsford <langz020@gmail.com>
2015-05-09 12:52:14 +0200

    added host form mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => "178.62.66.208" }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
