e531d6d94560813c2d28f6d7ce29489b14410b4b
ChrisLangsford <langz020@gmail.com>
2015-05-09 13:21:03 +0200

    mailer issue fixed

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -4,7 +4,7 @@ class RegistrationMailer < ActionMailer::Base
   def welcome_email(user)
   	@user = user
   	@url = root_url
-  	attachments.inline['logo.png'] = File.read('#{RAILS_ROOT}/app/assets/images/logo.png')
+  	
 
   	mail(to: @user.email, subject: 'Welcome to DojoTube')
   end
