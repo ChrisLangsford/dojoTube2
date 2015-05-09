ebcb1fe64b5b97a4aff3ebd61c3d18cf52d112be
ChrisLangsford <langz020@gmail.com>
2015-05-09 11:55:01 +0200

    added mailer settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => 'dojotube.co.za', # your domain to identify your server when connecting
+  }
+
 end
